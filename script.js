2ccc1e98fcc17bc6385f398d262a19115545dc47
MahAzevedo <madavetto@gmail.com>
2024-04-02 20:07:20 -0300

    new file: index.html & script

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = {
+    seletor: '.principal',
+}
+
+console.log(menu.seletor.toUpperCase());
+
+// function upperName(name) {
+//     return name.toUpperCase();
+// }
+
+// console.log(upperName('Mádjan'));
+
+
+const upperName = (name) => {
+    return name.toUpperCase();
+};
+
+console.log(upperName('Mádjan'))
