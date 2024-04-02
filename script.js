6b23f0a2cb87332ff4d71f77a30d58981d2e38fd
MahAzevedo <madavetto@gmail.com>
2024-04-02 20:31:09 -0300

    modified: script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const menu = {
-    seletor: '.principal',
-}
+// const menu = {
+//     seletor: '.principal',
+// }
 
-console.log(menu.seletor.toUpperCase());
+// console.log(menu.seletor.toUpperCase());
+//----------------------
+
 
 // function upperName(name) {
 //     return name.toUpperCase();
@@ -10,9 +12,27 @@ console.log(menu.seletor.toUpperCase());
 
 // console.log(upperName('Mádjan'));
 
+//----------------------
 
-const upperName = (name) => {
-    return name.toUpperCase();
-};
 
-console.log(upperName('Mádjan'))
+// const upperName = (name) => {
+//     return name.toUpperCase();
+// };
+
+
+//--------------------------
+
+const upperName = (name) => name.toUpperCase();
+
+console.log(upperName('Mádjan'));
+//-------------------------
+
+
+function handleMouse(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    console.log(x, y);
+}
+
+document.addEventListener('click', handleMouse);
