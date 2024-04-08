76616b9b9232dfb3bae3bdc9bf8ab1e6426af125
MahAzevedo <madavetto@gmail.com>
2024-04-07 23:30:31 -0300

    modified: index.html & script.js

--- script.js
+++ script.js
@@ -28,13 +28,33 @@ console.log(upperName('Mádjan'));
 //-------------------------
 
 
-function handleMouse(event) {
-    const { clientX, clientY } = event;
+// function handleMouse(event) {  // isso aqui é uma  desestruturação
+function handleMouse({ clientX, clientY }) {
+    // const { clientX, clientY } = event;
 
-    console.log(x, y);
+    console.log(clientX, clientY);
 }
 
 document.addEventListener('click', handleMouse);
 // tudo é objeto
 // null e o undefined não é um objeto
 // react 
+
+
+
+// array : desestruturação de uma array de frutas
+// isso aqui é funcão nova do React: que são os REACTS ROUTES, ele ffunciona logo de cara com a desestruturação. Ele RETORNA VALOR e uma FUNÇÃO e que ATUALIZA O VALOR 
+const frutas = ['banana', 'uva'];
+const [fruta1, fruta2] = frutas;
+console.log(fruta1)
+
+const useQuadrado = [
+    4,
+    function (lado) {
+        return 4 * lado;
+    },
+];
+
+const [lados, perimetro] = useQuadrado;
+console.log(lados);
+console.log(perimetro(5));
