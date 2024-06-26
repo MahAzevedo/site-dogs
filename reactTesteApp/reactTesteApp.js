e51e9f31d0afb636a2a5b5bcffe93a21e7b643bf
MahAzevedo <madavetto@gmail.com>
2024-06-26 01:29:47 -0300

    modified: reactTesteApp/reactTesteApp.js & modified: testeReact.js

---
+++ reactTesteApp/reactTesteApp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+    const produtos = ['Notebook', 'Tablet', 'Smartphone'];
+    return <p>{produtos}</p>
+};
+
+export default App;
--- testeReact.js
+++ testeReact.js
@@ -52,4 +52,5 @@ const App = () => {
     );
 };
 
-export default App; 
+export default App;
+
