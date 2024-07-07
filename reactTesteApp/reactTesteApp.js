f512243d402f507d70605bfca2ea3f1dccc9b1e3
MahAzevedo <madavetto@gmail.com>
2024-07-06 22:28:57 -0300

    modified: index.html & reactTesteApp/reactTesteApp.js

--- reactTesteApp/reactTesteApp.js
+++ reactTesteApp/reactTesteApp.js
@@ -1,30 +1,4 @@
-import React from 'react';
 
-const App = () => {
-    const produtos = ['Notebook', 'Tablet', 'Smartphone'];
-    return <p>{produtos}</p>
-};
-
-export default App;
-
-
-
-
-
-
-//************ 
-
-// FechamentoPedido
-// condição no if-then-else (abaixo) ou switch-case:
-
-if (brokerConfigurado == BROKER_1)
-    broker1.efetuarPagamento();
-else if (brokerConfigurado == BROKER_2)
-    broker2.efetuarPagamento();
-else if (brokerConfigurado == BROKER_3)
-    broker3.efetuarPagamento();
-
-// Solucao sem poliformismo
 
 
 
