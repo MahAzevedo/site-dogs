dd07cd69d7f37014a5a92fec08a18e159cf17321
MahAzevedo <madavetto@gmail.com>
2024-07-09 15:08:45 -0300

    modified: reactTesteApp/reactTesteApp.js

--- reactTesteApp/reactTesteApp.js
+++ reactTesteApp/reactTesteApp.js
@@ -1,4 +1,30 @@
+import React from 'react';
 
+const App = () => {
+    const produtos = ['Notebook', 'Tablet', 'Smartphone'];
+    return <p>{produtos}</p>
+};
+
+export default App;
+
+
+
+
+
+
+//************ 
+
+// FechamentoPedido
+// condição no if-then-else (abaixo) ou switch-case:
+
+if (brokerConfigurado == BROKER_1)
+    broker1.efetuarPagamento();
+else if (brokerConfigurado == BROKER_2)
+    broker2.efetuarPagamento();
+else if (brokerConfigurado == BROKER_3)
+    broker3.efetuarPagamento();
+
+// Solucao sem poliformismo
 
 
 
@@ -25,3 +51,10 @@ const produtos = [
     }
 ];
 
+const App = () => {
+    return <section>
+
+    </section>;
+};
+
+export default App;
