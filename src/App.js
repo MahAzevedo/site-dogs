439c9105c64729645b5112cd49be513fd56fc733
MahAzevedo <madavetto@gmail.com>
2024-07-23 18:27:01 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -51,9 +51,14 @@ const App = () => {
     const [contar, setContar] = React.useState(1);
     const [items, setItems] = React.useState(['Item 1']);
 
+    function handleClick() {
+        setContar((contar) => {
+            return contar + 1;
+        })
+    };
     return (
         <div>
-
+            <button onClick={handleClick}>{contar}</button>
         </div>
     )
 }
