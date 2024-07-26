bc006da4a79489b353cd0372ecff273386fe6fd3
MahAzevedo <madavetto@gmail.com>
2024-07-26 18:27:32 -0300

    modified:   Hooks/App.js & Hooks/index.js & Hooks/useFetch.js

--- Hooks/App.js
+++ Hooks/App.js
@@ -4,31 +4,37 @@ import useFetch from './useFetch';
 
 const App = () => {
     const [produto, setProduto] = useLocalStorage('produto', '');
-    const { request, data, loading } = useFetch();
+    const { request, data, loading, error } = useFetch();
 
     React.useEffect(() => {
-        request('https://ranekapi.origamid.dev/json/api/produto/')
-    }, []);
+        async function fetchData() {
+            const { response, json } = await request('https://ranekapi.origamid.dev/json/api/produto/');
+            console.log(response, json);
+        }
+        fetchData();
+    }, [request]);
 
     function handleClick({ target }) {
         setProduto(target.innerText);
     };
 
-    if (loading) return <p>Carregando...</p>
-    if (data === null) return null;
-    return (
-        <div>
-            <p>Produto preferido: {produto}</p>
-            <button onClick={handleClick}>Notebook</button>
-            <button onClick={handleClick}>Smartphone</button>
+    if (error) return <p>{error}</p>;
+    if (loading) return <p>Carregando...</p>;
+    if (data)
+        return (
+            <div>
+                <p>Produto preferido: {produto}</p>
+                <button onClick={handleClick}>Notebook</button>
+                <button onClick={handleClick}>Smartphone</button>
 
-            {data.map((produto) => (
-                <div key={produto.id}>
-                    <h1>{produto.nome}</h1>
-                </div>
-            ))}
-        </div>
-    );
+                {data.map((produto) => (
+                    <div key={produto.id}>
+                        <h1>{produto.nome}</h1>
+                    </div>
+                ))}
+            </div>
+        );
+    else return null;
 };
 
 export default App;
--- Hooks/useFetch.js
+++ Hooks/useFetch.js
@@ -5,13 +5,24 @@ const useFetch = () => {
     const [error, setError] = React.useState(null);
     const [loading, setLoading] = React.useState(null);
 
-    async function request(url, options) {
-        setLoading(true);
-        const response = await fetch(url, options);
-        const json = await response.json();
-        setData(json);
-        setLoading(false);
-    }
+    const request = React.useCallback(async (url, options) => {
+        let response;
+        let json;
+        try {
+            setError(null);
+            setLoading(true);
+            response = await fetch(url, options);
+            json = await response.json();
+            setLoading(false);
+        } catch (erro) {
+            json = null;
+            setError('erro');
+        } finally {
+            setData(json);
+            setLoading(false);
+            return { response, json }
+        }
+    }, []);
 
 
     return { data, error, loading, request };
