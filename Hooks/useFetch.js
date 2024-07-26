94097195b9547aac49947d28401cdac6b65b8dbc
MahAzevedo <madavetto@gmail.com>
2024-07-26 01:38:33 -0300

    new file:   Hooks/useFetch.js

---
+++ Hooks/useFetch.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const useFetch = () => {
+    const [data, setData] = React.useState(null);
+    const [error, setError] = React.useState(null);
+    const [loading, setLoading] = React.useState(null);
+
+    async function request(url, options) {
+        const response = await fetch(url, options);
+        const json = await response.json();
+        setData(json);
+    }
+
+    return { data, error, loading }
+};
+
+export default useFetch;
+
+
+// [ com COLCHETES é ARRAY ]
+// { com CHAVES É OBJETO }
