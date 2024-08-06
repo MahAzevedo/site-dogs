2f44304074de6453c81842a2f8914952e72fc9f5
MahAzevedo <madavetto@gmail.com>
2024-08-06 18:41:47 -0300

    modified:   input-Exercicio/App.js

--- input-Exercicio/App.js
+++ input-Exercicio/App.js
@@ -14,10 +14,19 @@ const App = () => {
 
     function handlechange() {
         const { id, value } = target;
+        setForm({ ...form, [id]: value })
     };
+
     return <form>
         <label htmlFor='nome'>Nome</label>
         <input type='text' id='nome' value={form.nome} onChange={handlechange} />
+        <label htmlFor='email'>E-mail</label>
+        <input
+            type='email'
+            id='email'
+            value={form.email}
+            onChange={handlechange}
+        />
     </form>
 };
 
