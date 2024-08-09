d231412fc1dd05fb79e262128c596e0fd74f69b4
MahAzevedo <madavetto@gmail.com>
2024-08-09 19:13:56 -0300

    modified:   checkbox/App.js

--- checkbox/App.js
+++ checkbox/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const App = () => {
+    const [termos, setTermos] = React.useState(false);
     return (
         <form>
             <label htmlFor=" ">
                 <input type="checkbox" />
-                Li os termos.
+                Aceito os termos.
             </label>
-
         </form>
-    )
-}
+    );
+};
 
-export default App
+export default App;
