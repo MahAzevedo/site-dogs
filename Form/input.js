abcbcdffb513694e885240fe0f198ae4c05fea2a
MahAzevedo <madavetto@gmail.com>
2024-08-14 20:29:10 -0300

    new file:   Form/input.js

---
+++ Form/input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    const [] = React.useState('');
+    return (
+        <form>
+
+        </form>
+    );
+};
+
+export default App;
+
+// A ideia aqui é otimizar (reciclar) componentes, se são repetidos dá para usar mais vezes 
