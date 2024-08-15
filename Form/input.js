f638bc5469f2c715e57402afa49a6dbe4e877556
MahAzevedo <madavetto@gmail.com>
2024-08-15 18:09:17 -0300

    new file:   Form/Select.js & Select-input/App.js e etc

--- Form/input.js
+++ Form/input.js
@@ -1,26 +1,29 @@
 import React from 'react'
 
-const Input = ({ id, label, setValue, value }) => {
+const Input = ({ id, label, setValue, ...props }) => {
 
     return (
-        <div>
+        <>
             <label htmlFor="id">{label}</label>
             <input
                 type="text"
                 id={id}
                 name={id}
-                value={value}
                 onChange={({ target }) => setValue = (target.value)}
+                {...props}
             />
-        </div>
+        </>
     );
 };
 
 export default Input;
 
-// A ideia aqui é otimizar (reciclar) componentes, se são repetidos dá para usar mais vezes 
+// A ideia aqui é otimizar (reciclar) componentes, se são repetidos dá para usar mais vezes
 
+// linhas 6 e 15: em vez de usar uma <div> usei o <></> fragmento, que nada mais é do que uma tag vazia, escolhida para evitar criar uma <div> desnecessária.
+// ou ainda nas:
+// linhas 6 e 15: se envolver em <p> ele cria um espaçamento já
 
-
+// para desestruturar se usa os três pontos assim-> ...
 
 
---
+++ Select-input/Select.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Select = () => {
+    return (
+        <select>
+
+        </select>
+    )
+}
+
+export default Select
