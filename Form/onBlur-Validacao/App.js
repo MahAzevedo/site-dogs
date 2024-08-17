673c3d2009126cdbbd94bcbe26e3657dbba3641c
MahAzevedo <madavetto@gmail.com>
2024-08-17 19:32:41 -0300

    new file: Form/onBlur-Validacao/App.js

---
+++ Form/onBlur-Validacao/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Input from './Form/Input';
+
+const App = () => {
+    return (
+        <form>
+            <Input />
+        </form>
+    );
+};
+
+export default App;
