9c9f6aeb90ebaf35d6a6254a9fbd569771d0836b
MahAzevedo <madavetto@gmail.com>
2024-08-18 12:38:07 -0300

    modified: Form/onBlur-Validacao/App.js

--- Form/onBlur-Validacao/App.js
+++ Form/onBlur-Validacao/App.js
@@ -2,9 +2,17 @@ import React from 'react'
 import Input from './Form/Input';
 
 const App = () => {
+    const [cep, setCep] = React.useState('');
     return (
         <form>
-            <Input />
+            <Input
+                label="CEP"
+                id="cep"
+                type="text"
+                value={cep}
+                setValue={setCep}
+                placeholder="00000-000"
+            />
         </form>
     );
 };
