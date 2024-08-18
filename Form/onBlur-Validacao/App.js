b1aca4858d53e19f20c4c1b8fc6149e5cd7019b7
MahAzevedo <madavetto@gmail.com>
2024-08-18 12:46:22 -0300

    modified: Form/onBlur-Validacao/App.js

--- Form/onBlur-Validacao/App.js
+++ Form/onBlur-Validacao/App.js
@@ -3,6 +3,11 @@ import Input from './Form/Input';
 
 const App = () => {
     const [cep, setCep] = React.useState('');
+
+    function handleBlur({ target }) {
+        console.log(target.value);
+    };
+
     return (
         <form>
             <Input
@@ -11,10 +16,18 @@ const App = () => {
                 type="text"
                 value={cep}
                 setValue={setCep}
+                onBlur={handleBlur}
                 placeholder="00000-000"
             />
+            {cep}
         </form>
     );
 };
 
 export default App;
+
+// linha 14: VALIDAÇÃO::
+// ----->>> a validação acontece no onBlur!!!!!
+
+// eu quero validar esse valor, como eu faço?
+// a validação acontece no onBlur
