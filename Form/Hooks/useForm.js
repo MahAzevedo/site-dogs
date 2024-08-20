4e64a4a1e81d312a4e81dcb400c2bb07bb9f3d2e
MahAzevedo <madavetto@gmail.com>
2024-08-19 22:00:11 -0300

    modified: Form/Hooks/useForm.js

--- Form/Hooks/useForm.js
+++ Form/Hooks/useForm.js
@@ -10,3 +10,6 @@ export default useForm;
 
 // ::: Atenção :::
 // No HOOK não RETORNA ELEMENTO, o que retorna SÃO PARTES ESPECÍFICAS DO MEU HOOK
+
+// regex: regular expression, é o que significa
+// regex: regular expression JavaScript
