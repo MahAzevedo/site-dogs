8ee32a70927290772aa1f77e940f1c8791cc8055
MahAzevedo <madavetto@gmail.com>
2024-08-26 19:17:34 -0300

    modified:   Form/Hooks/useForm.js

--- Form/Hooks/useForm.js
+++ Form/Hooks/useForm.js
@@ -16,6 +16,7 @@ const useForm = (type) => {
     const [error, setError] = React.useState(null);
 
     function validate(value) {
+        if (type === false) return true;
         if (value.length === 0) {
             setError('Preencha um valor');
             return false;
