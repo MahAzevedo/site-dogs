3f6257bd51bed3ffedd4f24e261cddc1c31a7fae
MahAzevedo <madavetto@gmail.com>
2024-08-28 19:22:43 -0300

    modified: Form/Hooks/App.js

--- Form/Hooks/App.js
+++ Form/Hooks/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
     function handleSubmit(event) {
         event.preventDefault();
-        if (cep.validate() && email.validate() && nome.validade() && sobrenome.validate()) {
+        if (cep.validate() && email.validate() && nome.validade()) {
             console.log('Enviou');
         } else {
             console.log('Não enviar');
