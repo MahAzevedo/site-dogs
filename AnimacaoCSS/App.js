eef13dc0455c7943fa70bd7849ebe3852f37fe9c
MahAzevedo <madavetto@gmail.com>
2024-09-11 12:14:29 -0300

    modified: AnimacaoCSS/App.js

--- AnimacaoCSS/App.js
+++ AnimacaoCSS/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const App = () => {
+    const [ativar, setAtivar] = React.useState(false);
     return (
         <div>
+            <button>Ativar</button>
             <Produto />
         </div>
     );
