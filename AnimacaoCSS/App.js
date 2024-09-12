88d268854721d14821f4cb11bee1ef05bbed9bef
MahAzevedo <madavetto@gmail.com>
2024-09-12 12:17:23 -0300

    modified: AnimacaoCSS/App.js

--- AnimacaoCSS/App.js
+++ AnimacaoCSS/App.js
@@ -4,9 +4,9 @@ const App = () => {
     const [ativar, setAtivar] = React.useState(false);
     return (
         <div>
-            <button>Ativar</button>
-            <Produto />
-        </div>
+            <button onClick={() => setAtivar(!ativar)}>Ativar</button>
+            {ativar && <Produto />}
+        </div >
     );
 };
 
