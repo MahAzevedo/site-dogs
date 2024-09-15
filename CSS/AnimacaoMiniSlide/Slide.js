62d38609a0e6d918b699de8a352a9157c1b1e785
MahAzevedo <madavetto@gmail.com>
2024-09-15 18:13:13 -0300

    modified: CSS/AnimacaoMiniSlide/Slide.js

--- CSS/AnimacaoMiniSlide/Slide.js
+++ CSS/AnimacaoMiniSlide/Slide.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styles from './Slide.module.css';
 
 const Slide = ({ slides }) => {
     return (
-        <section>
-            {slides.map((slide) => (
-                <div key={slide.id}>{slide.text}</div>
-            ))};
+        <section className={Styles.container}>
+            <div className={Styles.content}>
+                {slides.map((slide) => (
+                    <div key={slide.id}>{slide.text}</div>
+                ))};
+            </div>
+            <nav>
+                <buttom>Anterior</buttom>
+                <buttom>Próximo</buttom>
+            </nav>
         </section>
     );
 };
