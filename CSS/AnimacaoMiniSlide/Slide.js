df08b2a2e6f02da17cfa985970dd798091e84964
MahAzevedo <madavetto@gmail.com>
2024-09-20 20:10:39 -0300

    modified:   CSS/AnimacaoMiniSlide/Slide.js

--- CSS/AnimacaoMiniSlide/Slide.js
+++ CSS/AnimacaoMiniSlide/Slide.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Slide.module.css';
+import { transform } from '@babel/core';
 
 const Slide = ({ slides }) => {
+    const [active, setActive] = React.useState(0);
+
     return (
         <section className={styles.container}>
-            <div className={styles.content}>
+            <div className={styles.content} style={{ transform: 'translateX()' }}>
                 {slides.map((slide) => (
                     <div key={slide.id} className={styles.item}>{slide.text}
                     </div>
