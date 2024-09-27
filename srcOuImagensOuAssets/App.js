df557e0f4340277a7f2981bb5932ada9856a2d1a
MahAzevedo <madavetto@gmail.com>
2024-09-26 21:09:18 -0300

    modified:   srcOuImagensOuAssets/App.css & App.js

--- srcOuImagensOuAssets/App.js
+++ srcOuImagensOuAssets/App.js
@@ -5,6 +5,7 @@ import foto from './img/foto.jpg';
 const App = () => {
     return (
         <div>
+            <p className="fundo"></p>
             <img src={foto} alt="Cachorro" />
         </div>
     );
