d57056de11bfc8d21fd7fee98f0d33581adba18b
MahAzevedo <madavetto@gmail.com>
2024-09-26 21:13:06 -0300

    modified:   srcOuImagensOuAssets/App.js

--- srcOuImagensOuAssets/App.js
+++ srcOuImagensOuAssets/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import foto from './img/foto.jpg';
+import dog from './img/dog.svg';
 
 const App = () => {
     return (
         <div>
             <p className="fundo"></p>
             <img src={foto} alt="Cachorro" />
+            <img src={dog} alt="Dog" />
         </div>
     );
 };
