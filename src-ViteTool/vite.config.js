8b8f8077d6505a7e1c3f437caccac599b737a73a
MahAzevedo <madavetto@gmail.com>
2024-09-27 21:35:54 -0300

    modified:   src-ViteTool/vite.config.js

--- src-ViteTool/vite.config.js
+++ src-ViteTool/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-eslint';
 import eslintPlugin from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
             cache: false,
             include: ['./src/**/*.js', './src/**/*.jsx'],
         }),
+        svgr(),
     ],
 });
