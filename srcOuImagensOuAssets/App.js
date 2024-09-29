9125c79d1ccdc2716f6190faa1a9d3320e0f91e5
MahAzevedo <madavetto@gmail.com>
2024-09-28 21:53:48 -0300

    by writing ReactComponent as something, you can repeat the ReactComponent as many times as necessary

--- srcOuImagensOuAssets/App.js
+++ srcOuImagensOuAssets/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import foto from './img/foto.jpg';
-import { ReactComponent } from './img/dog.svg';
+import { ReactComponent as Dog } from './img/dog.svg';
 
 const App = () => {
     return (
         <div>
             <p className="fundo"></p>
             <ReactComponent />
-            <img src={dog} alt="Dog" />
             <img src={foto} alt="Cachorro" />
         </div>
     );
