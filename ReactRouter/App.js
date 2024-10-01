a50505c9e12bb426ec9b8bf981a6642877a66e43
MahAzevedo <madavetto@gmail.com>
2024-10-01 16:55:31 -0300

    new file:   ReactRouter/Header.js & NaoEncontrada.js & Sobre.js

--- ReactRouter/App.js
+++ ReactRouter/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Sobre from './Sobre';
+import Header from './Header';
+import NaoEncontrada from './NaoEncontrada';
+import Footer from './Footer';
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
-                <Route />
+                <Route path="/" element={<Home />} />
+                <Route path="Sobre" element={<Sobre />} />
+                <Route path="*" element={<NaoEncontrada />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     );
 };
 
 export default App;
+
+
+// Dentro do Route, coloca path="" -->> <Route path="/" />   esse é o caminho da HOME
+
+// HOME -->> é só colocar barra/ que tá indicando o caminho da HOME
+// quando tiver o barra/ significa que é o caminho para HOME
