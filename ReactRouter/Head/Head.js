256f6c1e2814aff8f2e244e45488e19631915cde
MahAzevedo <madavetto@gmail.com>
2024-10-01 23:56:01 -0300

    modified:   ReactRouter/Head/Head.js

--- ReactRouter/Head/Head.js
+++ ReactRouter/Head/Head.js
@@ -26,4 +26,11 @@ export default Head;
  Um Hook é assim -->>> React.useEffect(), isso é um hook
 
  Como usamos o React, não temos acesso ao Head do site, pra acessá-lo dá para usar um componente ou um CustonHook, não tem diferença, que vai receber propriedade e essa propriedade no useEffect(() => {}) vai modificar o título que eu quiser e modificar qualquer meta tag que eu quiser utilizando JavaScript puro mesmo.
+
+
+ ** Forma de fazer do zero sem dependência de nenhuma biblioteca externa **
+
+  O description é o que informa sobre do que se trata o site
+
+  E coloca no topo da página o nome do site
 */
