2dcd105eb9aa1ae1e0ef61037f0abe710f80e1db
MahAzevedo <madavetto@gmail.com>
2024-10-18 00:00:56 -0300

    modified: PropTypes/src/Button.js

--- PropTypes/src/Button.js
+++ PropTypes/src/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = (props) => {
   return (
-    <button >{props.children}</button>
+    <button style={{width: `${props.width}`}}>{props.children}</button>
   );
 };
 
